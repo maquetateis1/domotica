ed899082849b5a11a4ac1a1791f222c4e677abe3
maquetateis1 <maqueta1@iesteis.net>
2025-05-09 14:25:36 +0200

    Update P1maqueta.py

--- programas/P1maqueta.py
+++ programas/P1maqueta.py
@@ -1,27 +1,29 @@
-"""Programa que acende leds en vermello se a temperatura pasa de 20º
-Autor: Bernardo Álvarez
-Data: 20/040/2025"""
-
 from microbit import *
 import neopixel
 
-  # 2 LED neopixel conectados ao pin 13
+# Configuración del Neopixel en el pin 13
 
-led = pin14 # LED branco conectado ao pin14
+np = neopixel.NeoPixel(pin13, 2)  # 2 LED conectado al pin 13
+# Configuración del LED normal en el pin 16
+led_normal = pin16
 np.clear()
+
 while True:
-    temperatura = temperature() # gardamos valor da temperatura
+    # Leer la temperatura
+    temperatura = temperature()
 
+    # Si la temperatura es mayor a 28 grados, encender los LEDs
     if temperatura > 15:
-        np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
-        np[1] = (0, 255, 0)
-        np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(1)  # Acender o LED normal
+        np[0] = (255, 0, 0)  # Encender el Neopixel 1 en rojo
+        np[1] = (255, 0, 0)  # Encender el Neopixel 2 en rojo
+        np.show()  # Mostrar el color en el LED
+        led_normal.write_digital(1)  # Encender el LED normal
     else:
-        np[0] = (255, 0, 0)  # Apagar os Neopixel
-        np[1] = (255, 0, 0)  # Acender os Neopixel en verde
-        np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(0)  # Apagar o LED normal
+        np[0] = (0, 0, 0)  # Apagar el Neopixel 1
+        np[1] = (0, 0, 0)  # Apagar el Neopixel 2
+        np.show()  # Mostrar 
+        led_normal.write_digital(0)  # Apagar el LED normal
 
-    sleep(1000)  # Esperar 1 segundo
+    sleep(1000)  # Esperar 1 segundo antes de volver a comprobar la temperatura
+
 
