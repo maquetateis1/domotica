c9b2665ca227c0b583429cdd15db53c20d69a4b7
SantiVigo <pereirasanti3008@gmail.com>
2025-05-19 09:48:35 +0200

    Update P4maqueta.py

--- programas/P4maqueta.py
+++ programas/P4maqueta.py
@@ -1,36 +1,24 @@
 """Programa completo da fila 1 para a maqueta domótica.
 Autores: Santiago Pereira 
 Data: 30/04/2025"""
-
 from microbit import *
-import machine
+import time
 
-# El servo está conectado a pin0
-servo = machine.PWM(machine.Pin(0))
-servo.freq(50)
+# Inicializar el servo en el pin0
+pin0.set_analog_period(20) # Período 20ms
 
-# Función para poner ángulo en servo (0º a 180º)
-# Servo típico usa duty entre ~26 (0º) y ~128 (180º) en periodo 20ms (freq 50Hz)
-# Para 0º -> duty=26, para 90º duty=77 aprox.
-
-def set_angle(angle):
-    duty = 26 + (angle * (128-26)) // 180
-    servo.duty(duty)
-
-# Estado inicial
 angle = 0
-set_angle(angle)
-
-button_b_last = False
 
 while True:
-    button_b = button_b.is_pressed()
-    if button_b and not button_b_last:
-        # Cambiar ángulo
-        if angle == 0:
-            angle = 90
-        else:
-            angle = 0
-        set_angle(angle)
-    button_b_last = button_b
-    sleep(50)
+    # Calcular d en línea
+pulse_ms = 0.5 + (angle / 180.0) * 2.0
+duty = int((pulse_ms / 20) * 1023)
+pin0.write_analog(duty)
+sleep(100)
+
+   if button_b.was_pressed():
+# Incrementar angulo en 10 hasta 90, luego 0
+   if angle < 90:
+angle += 10
+   else:
+angle = 0
