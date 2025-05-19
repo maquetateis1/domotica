7ae931e2729fe90e2fc36760112783c48a9bdcc9
HuSeViGo <fargoughousain@gmail.com>
2025-05-19 09:29:19 +0200

    Update programa_completo1.py

--- programas/programa_completo1.py
+++ programas/programa_completo1.py
@@ -6,12 +6,21 @@ Data:
 from microbit import *
 import music
 from machine import Pin
+import music
+import neopixel
 
 # Definir o LED no pin 14
 led_pin = Pin(14, Pin.OUT)
 # El servo está conectado a pin0
 servo = machine.PWM(machine.Pin(0))
 servo.freq(50)
+# PIR sensor conectado a pin0
+pir = pin0
+# Tira de neopixels conectada a pin1 con 8 LEDs
+np = neopixel.NeoPixel(pin1, 8)
+# Colores
+RED = (255, 0, 0)
+OFF = (0, 0, 0)
 
 while True:
     if button_a.was_pressed():
@@ -42,3 +51,34 @@ while True:
         set_angle(angle)
     button_b_last = button_b
     sleep(50)
+
+while True:
+    if pir.read_digital() == 1:  # presencia detectada
+        # Reproducir ringtone dos veces
+        music.play(music.POWER_UP)
+        music.play(music.POWER_UP)
+        # Parpadear neopixels rojos 5 veces periodo 500ms
+        for _ in range(5):
+            for i in range(len(np)):
+                np[i] = RED
+            np.show()
+            sleep(500)
+            for i in range(len(np)):
+                np[i] = OFF
+            np.show()
+            sleep(500)
+        # Parpadear LED blanco 5 veces periodo 500ms
+        for _ in range(5):
+            display.set_all(9)  # encender LED blanco (máxima intensidad)
+            sleep(500)
+            display.set_all(0)  # apagar LED blanco
+            sleep(500)
+        # Mostrar cara enfadada 5 veces en la matriz 500ms ON y 500ms OFF
+        for _ in range(5):
+            display.show(Image.ANGRY)
+            sleep(500)
+            display.clear()
+            sleep(500)
+    else:
+        display.show(Image.HOUSE)  # mostrar casa cuando no hay presencia
+        sleep(100)
