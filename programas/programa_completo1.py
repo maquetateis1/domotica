b83aa1d7f4f57b882b69b60902d0640592424b5c
HuSeViGo <fargoughousain@gmail.com>
2025-05-19 09:30:19 +0200

    Update programa_completo1.py

--- programas/programa_completo1.py
+++ programas/programa_completo1.py
@@ -9,6 +9,10 @@ from machine import Pin
 import music
 import neopixel
 
+np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
+
+led = pin14 # LED branco conectado ao pin14
+np.clear()
 # Definir o LED no pin 14
 led_pin = Pin(14, Pin.OUT)
 # El servo está conectado a pin0
@@ -82,3 +86,19 @@ while True:
     else:
         display.show(Image.HOUSE)  # mostrar casa cuando no hay presencia
         sleep(100)
+
+while True:
+    temperatura = temperature() # gardamos valor da temperatura
+
+    if temperatura > 20:
+        np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
+        np[1] = (0, 255, 0)
+        np.show()  # Mostrar a cor nos neopixel
+        led.write_digital(1)  # Acender o LED normal
+    else:
+        np[0] = (255, 0, 0)  # Apagar os Neopixel
+        np[1] = (255, 0, 0)  # Acender os Neopixel en verde
+        np.show()  # Mostrar a cor nos neopixel
+        led.write_digital(0)  # Apagar o LED normal
+
+    sleep(1000)  # Esperar 1 segundo
