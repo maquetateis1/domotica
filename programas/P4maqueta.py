9d626c9c0b1ef5cc09fccba6e5968736e71473c0
SantiVigo <pereirasanti3008@gmail.com>
2025-05-20 14:01:26 +0200

    Update P4maqueta.py

--- programas/P4maqueta.py
+++ programas/P4maqueta.py
@@ -1,24 +1,40 @@
 """Programa completo da fila 1 para a maqueta domótica.
 Autores: Santiago Pereira 
-Data: 30/04/2025"""
+Data: 30/04/2025
+"""
+
 from microbit import *
-import time
+import machine
 
-# Inicializar el servo en el pin0
-pin0.set_analog_period(20) # Período 20ms
+# Constants for servo control
+# For micro:bit PWM, period 20 ms (50 Hz), duty cycle in microseconds.
+# We set the PWM directly using machine.PWM
 
-angle = 0
+servo_pin = machine.PWM(machine.Pin(0))
+servo_pin.freq(50)
+
+# Function to convert angle to duty for micro:bit PWM: 
+# We will map pulse width from 0.5 ms (500 us) to 2.5 ms (2500 us)
+# but microbit PWM duty is 16 bit range and period 20 ms (20000 us)
+# duty = pulse_us * max_duty / period_us
+
+def angle_to_duty(angle):
+    period = 20000  # microseconds (20ms)
+    min_pulse = 500  # 0 degree pulse width in microseconds
+    max_pulse = 2500  # 180 degree pulse width in microseconds
+    pulse = min_pulse + (max_pulse - min_pulse) * angle / 180
+    duty = int(pulse * 65535 / period)
+    return duty
+
+# Start with servo at 0 degrees
+position = 0
+servo_pin.duty_u16(angle_to_duty(position))
 
 while True:
-    # Calcular d en línea
-pulse_ms = 0.5 + (angle / 180.0) * 2.0
-duty = int((pulse_ms / 20) * 1023)
-pin0.write_analog(duty)
-sleep(100)
-
-   if button_b.was_pressed():
-# Incrementar angulo en 10 hasta 90, luego 0
-   if angle < 90:
-angle += 10
-   else:
-angle = 0
+    if button_b.was_pressed():
+        if position == 0:
+            position = 90
+        else:
+            position = 0
+        servo_pin.duty_u16(angle_to_duty(position))
+    sleep(10)
