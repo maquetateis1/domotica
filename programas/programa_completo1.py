74d78c55dff5c0a9f9813c57dccfc7f184f00370
HuSeViGo <fargoughousain@gmail.com>
2025-05-21 14:11:28 +0200

    Update programa_completo1.py

--- programas/programa_completo1.py
+++ programas/programa_completo1.py
@@ -3,56 +3,43 @@ Autores:
 Data: 
 """
 from microbit import *
-from machine import Pin
-import music
-import music
 import neopixel
-import time
+from microbit import *
+import music
+from machine import 
 
-np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
-
-led = pin14 # LED branco conectado ao pin14
-np.clear()
+servo_pin = machine.PWM(machine.Pin(0))
+servo_pin.freq(50)
 # Definir o LED no pin 14
 led_pin = Pin(14, Pin.OUT)
-# El servo está conectado a pin0
-servo = machine.PWM(machine.Pin(0))
-servo.freq(50)
-# Inicializar el servo en el pin0
-pin0.set_analog_period(20)  # Período 20ms (50Hz)
-angle = 0
-# PIR sensor conectado a pin0
+np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
+rele = pin16 # LED branco conectado ao pin14
+np.clear()
+#PIR sensor conectado a pin0
 pir = pin0
-# Tira de neopixels conectada a pin1 con 8 LEDs
+#Tira de neopixels conectada a pin1 con 8 LEDs
 np = neopixel.NeoPixel(pin1, 8)
 # Colores
 RED = (255, 0, 0)
 OFF = (0, 0, 0)
 
-while True:
-    if button_a.was_pressed():
-        led_pin.write_digital(1)  # Acende o LED conectado ao pin 14
-        music.play(music.RINGTONE)  # Reproduce o son RINGTONE
-        sleep(5000)  # Espera 5 segundos
-        led_pin.write_digital(0)  # Apaga o LED
-        
-while True:
-    # Calcular d en línea
-    pulse_ms = 0.5 + (angle / 180.0) * 2.0
-    duty = int((pulse_ms / 20) * 1023)
-    pin0.write_analog(duty)
-    sleep(100)
-
-    if button_b.was_pressed():
-        # Incrementar angulo en 10 hasta 90, luego 0
-        if angle < 90:
-            angle += 10
-        else:
-            angle = 0
-
 
 while True:
-    if pir.read_digital() == 1:  # presencia detectada
+    temperatura = temperature() # gardamos valor da temperatura
+
+    if temperatura > 20:
+        np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
+        np[1] = (0, 255, 0)
+        np.show()  # Mostrar a cor nos neopixel
+        rele.write_digital(0)  # Acender o LED normal
+    else:
+        np[0] = (255, 0, 0)  # Apagar os Neopixel
+        np[1] = (255, 0, 0)  # Acender os Neopixel en verde
+        np.show()  # Mostrar a cor nos neopixel
+        rele.write_digital(1)  # Apagar o LED normal
+
+    sleep(100)  # Esperar 1 segundo
+     if pir.read_digital() == 1:  # presencia detectada
         # Reproducir ringtone dos veces
         music.play(music.POWER_UP)
         music.play(music.POWER_UP)
@@ -76,24 +63,33 @@ while True:
         for _ in range(5):
             display.show(Image.ANGRY)
             sleep(500)
+            def angle_to_duty(angle):
+    period = 20000  # microseconds (20ms)
+    min_pulse = 500  # 0 degree pulse width in microseconds
+    max_pulse = 2500  # 180 degree pulse width in microseconds
+    pulse = min_pulse + (max_pulse - min_pulse) * angle / 180
+    duty = int(pulse * 65535 / period)
+    return duty
+
+# Start with servo at 0 degrees
+position = 0
+servo_pin.duty_u16(angle_to_duty(position))
+
+while True:
+    if button_b.was_pressed():
+        if position == 0:
+            position = 90
+        else:
+            position = 0
+        servo_pin.duty_u16(angle_to_duty(position))
+    sleep(10)  
             display.clear()
             sleep(500)
     else:
         display.show(Image.HOUSE)  # mostrar casa cuando no hay presencia
         sleep(100)
-
-while True:
-    temperatura = temperature() # gardamos valor da temperatura
-
-    if temperatura > 20:
-        np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
-        np[1] = (0, 255, 0)
-        np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(1)  # Acender o LED normal
-    else:
-        np[0] = (255, 0, 0)  # Apagar os Neopixel
-        np[1] = (255, 0, 0)  # Acender os Neopixel en verde
-        np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(0)  # Apagar o LED normal
-
-    sleep(1000)  # Esperar 1 segundo
+    if button_a.was_pressed():
+        led_pin.write_digital(1)  # Acende o LED conectado ao pin 14
+        music.play(music.RINGTONE)  # Reproduce o son RINGTONE
+        sleep(5000)  # Espera 5 segundos
+        led_pin.write_digital(0)  # Apaga o LED
