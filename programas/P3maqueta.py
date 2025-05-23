fc3fff446cf917400c0566aa67bd841e7bcda61e
SantiVigo <pereirasanti3008@gmail.com>
2025-05-23 18:11:48 +0200

    Update P3maqueta.py

--- programas/P3maqueta.py
+++ programas/P3maqueta.py
@@ -1,1 +1,23 @@
 
+
+Copyimport time
+import board
+import digitalio
+import audioio
+
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+
+button_a = digitalio.DigitalInOut(board.BUTTON_A)
+button_a.switch_to_input(pull=digitalio.Pull.DOWN)
+
+wave_file = open("RINGTONE.wav", "rb")
+ringtone = audioio.WaveFile(wave_file)
+
+while True:
+    if button_a.value:
+        led.value = True
+        audioio.AudioOut(board.A0, ringtone).play()
+        time.sleep(5)
+        led.value = False
+
